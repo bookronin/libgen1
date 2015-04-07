e56f416dbcd5105e371ffc0b22c05cc09760be8f
fistamoon1 <linoge67@yandex.ru>
2015-04-07 11:56:29 +0400

    Rejected commit: Default

--- libgen.js
+++ libgen.js
@@ -1,8 +1,23 @@
-var str ="Текст ()(от лат. textus — «ткань; сплетение, связь, сочетание»)";
-var reg = new RegExp ('[!@#$>%^}&*):;?,(—<{;».«]','gi');
-var dif = str.replace ( reg,' ');
 
-var chunk= dif.split(" ").sort();
+var userlib ="Повествование, в отличие от описания, представляет собой изображение событий или явлений, совершающихся не одновременно, а следующих друг за другом или обусловливающих друг друга. Самый, по-видимому, краткий в мировой литературе образец повествования – знаменитый рассказ Цезаря: «Пришёл, увидел, победил». Он ярко и точно передаёт суть повествования – это рассказ о том, что произошло, случилось."
+;
 
+var reg = new RegExp ('[!@`~#$>%^}&*):;?,1234567890–(—<{;».«\n]','gi');
+var dif = userlib.replace ( reg,' ');
 
-console.log(chunk);
+var uslib= dif.toLowerCase().split(" ").sort();
+
+/*for (var i = uslib.length; i >= 0; i--) {
+
+    console.log(uslib[i].parseFloat().length );
+    if (uslib[i].length == 1) uslib.splice(i, 1);
+}*/
+for (var j = 0 ; j < uslib.length; j++) {
+   if(uslib[j].length <= 2) uslib[j]='';
+}
+
+for (var i = uslib.length; i >= 0; i--) {
+    if (!uslib[i]) uslib.splice(i, 1);
+}
+;
+console.log(uslib);
